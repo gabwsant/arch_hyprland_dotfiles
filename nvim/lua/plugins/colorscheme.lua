41e9c6a85f2e8a83c137f176feef01ac56694ec2
Gabriel Barbosa Santos <gabrielsantos.gbs@outlook.com>
2026-01-31 14:29:20 -0300

    Add: MangoHud, gamemode, lazyvim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+  -- Instala o One Dark Pro
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+    opts = {
+      options = {
+        transparency = true,
+        cursorline = true,
+      },
+    }, 
+  },
+
+  -- Define o One Dark Pro como o tema padrão do LazyVim
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
